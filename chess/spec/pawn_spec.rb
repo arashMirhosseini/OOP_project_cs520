dec24be7b139ae806062c06cc6e7f59be6018016
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2019-06-05 11:01:14 -0700

    added tests to pawn

--- chess/spec/pawn_spec.rb
+++ chess/spec/pawn_spec.rb
@@ -4,12 +4,22 @@ require "board"
 
 
 describe Pawn do
-	let(:redPawn) { Pawn.new(:red,Board.new(false),[1,1]) }
+	let(:freshBoard) {Board.new}
+	let(:emptyBoard) {Board.new(false)}
+	let(:redPawnOnFreshBoard) { freshBoard[[1,1]] }
+	let(:redPawnOnEmptyBoard) { Pawn.new(:red, emptyBoard, [1,1]) }
 
 	describe "#move_forward_not_blocked" do
-		it "red pawn at [1,1] on fresh should be allowed to move forward" do
-			expect(redPawn.move_forward_not_blocked(2,0)).to eq([[3,1]])
+		it "red pawn at [1,1] on empty board should be allowed to move forward" do
+			expect(redPawnOnEmptyBoard.move_forward_not_blocked(2,0)).to eq([[3,1]])
 		end
 	end
+
+	describe "#move_forward_not_blocked" do
+		it "red pawn at [1,1] on empty board should not be allowd to move backword" do
+			expect(redPawnOnEmptyBoard.move_forward_not_blocked(-2,0)).to eq([])
+		end
+	end
+
 end
 
