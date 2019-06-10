910dc882e44d98aa9c2b7be15bd0fdcb3c07f68d
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2019-06-10 15:55:11 -0700

    refactored pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -40,13 +40,8 @@ class Pawn < Piece
   def try_forward_move(dx, dy)
     moves = []
 	new_position = self.position.zip([dx,dy]).map(&:sum)
-    if dx.abs != 2
+    if dx.abs != 2 || self.start_row?
       if @board.empty?(new_position) && @board.valid_pos?(new_position)
-        moves << new_position
-        position = new_position
-      end
-    else
-      if @board.empty?(new_position) && self.start_row?
         moves << new_position
         position = new_position
       end
